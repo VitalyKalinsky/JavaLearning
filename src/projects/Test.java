c5a910614ea9dd3ff3259626c3392956e064debc
Vizenp <kalinskyvi@gmail.com>
2022-09-04 19:54:55 +0300

    java NIO folder

--- src/projects/Test.java
+++ src/projects/Test.java
@@ -1,15 +1,12 @@
 package projects;
 
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.nio.file.Files;
-import java.nio.file.FileVisitResult;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.io.IOException;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.io.RandomAccessFile;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        
-    }   
+
+    }
 }
---
+++ src/projects/javaNIO/Main.java
@@ -0,0 +1,13 @@
+package projects.javaNIO;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
